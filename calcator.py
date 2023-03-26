8798a7fc6d7588c4cbff86ecb261d766301ccb60
afzalaham <126503021+afzalaham@users.noreply.github.com>
2023-03-27 00:12:32 +0530

    Add files via upload

---
+++ calcator.py
@@ -0,0 +1,8 @@
+# take user input for the expression to be evaluated
+expression = input("Enter an expression: ")
+
+# evaluate the expression using eval() function
+result = eval(expression)
+
+# print the result
+print("Result: ", result)
